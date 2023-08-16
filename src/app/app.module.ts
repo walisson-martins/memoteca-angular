adddbe1aace256b5330e39dacbcca1ac4ff7a515
Walisson Martins <wm.silva@apoioprodesp.sp.gov.br>
2023-08-16 14:55:15 -0300

    Adicionando formModule e ajustes de binding dos elementos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
-import { RodapeComponent } from './components/rodape/rodape.component';
 import { CriarPensamentosComponent } from './components/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CriarPensamentosComponent } from './components/pensamentos/criar-pensam
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-criar-pensamentos',
   templateUrl: './criar-pensamentos.component.html',
-  styleUrls: ['./criar-pensamentos.component.css']
+  styleUrls: ['./criar-pensamentos.component.css'],
 })
 export class CriarPensamentosComponent {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: '',
+  };
 
+  criarPensamento(): void {
+    alert('novo');
+  }
+
+  cancelar(): void {
+    
+  }
 }
